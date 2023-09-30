4bfd334fc40872dcd924fd7a0774bb377166ccfa
DESKTOP-596VCCI\Arkadiusz <arknie08576@wp.pl>
2023-09-30 17:29:33 +0200

    and everybody

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World and Arek and Adam!");
+Console.WriteLine("Hello, World and Arek and Adam and everybody!");
