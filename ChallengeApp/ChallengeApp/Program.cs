caa113fb22de7dfcce6bfa390c55b0b74bd5f6bf
DESKTOP-596VCCI\Arkadiusz <arknie08576@wp.pl>
2023-10-01 13:02:03 +0200

    Dzień 4 praca domowa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World and Arek and Adam and everybody!");
+
+
+string name = "Ewa";
+int age = 33;
+bool isWoman = true;
+
+if(isWoman == true&& age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+
+}else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+
+}else if (age < 18 && isWoman == false)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
