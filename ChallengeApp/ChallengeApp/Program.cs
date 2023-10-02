b78f2e93db9d9ec64643d92b1f8fce4b36ddd400
DESKTOP-596VCCI\Arkadiusz <arknie08576@wp.pl>
2023-10-02 12:21:16 +0200

    kod sformatowany

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -5,16 +5,19 @@ string name = "Ewa";
 int age = 33;
 bool isWoman = true;
 
-if(isWoman == true&& age < 30)
+if (isWoman == true && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 
-}else if (name == "Ewa" && age == 33)
+}
+else if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa, lat 33");
 
-}else if (age < 18 && isWoman == false)
+}
+else if (age < 18 && isWoman == false)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
+
 
