0b218e578f169b8043069f979dec49697a0c8067
DESKTOP-596VCCI\Arkadiusz <arknie08576@wp.pl>
2023-10-04 20:29:36 +0200

    dzień 6, praca domowa

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        string name { get; set; }
+        string surname { get; set; }
+        int age { get; set; }
+
+        public List<int> points { get; private set; }
+
+        public int Result { get { return points.Sum(); } }
+
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            points = new List<int>();
+        }
+        public void AddPoints(int score)
+        {
+            points.Add(score);
+        }
+        public void Print()
+        {
+            Console.WriteLine(name + " " + surname + ", wiek " + age);
+            Console.WriteLine("Wynik " + Result);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,25 +1,34 @@
 ﻿
-int number = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
+using ChallengeApp;
 
-Console.WriteLine("Wyniki dla liczby: " + number);
+Employee pracownik1 = new Employee("Tomasz", "Kot", 44);
+pracownik1.AddPoints(2);
+pracownik1.AddPoints(3);
+pracownik1.AddPoints(6);
+pracownik1.AddPoints(8);
+pracownik1.AddPoints(4);
+Employee pracownik2 = new Employee("Maciej", "Cichy", 24);
+pracownik2.AddPoints(5);
+pracownik2.AddPoints(5);
+pracownik2.AddPoints(7);
+pracownik2.AddPoints(3);
+pracownik2.AddPoints(2);
+Employee pracownik3 = new Employee("Paweł", "Typow", 37);
+pracownik3.AddPoints(7);
+pracownik3.AddPoints(5);
+pracownik3.AddPoints(9);
+pracownik3.AddPoints(6);
+pracownik3.AddPoints(5);
 
-for (int i = 0; i < 10; i++)
+if (pracownik1.Result > pracownik2.Result && pracownik1.Result > pracownik3.Result)
 {
-    int numberOfAppereances = 0;
-
-    foreach (char c in letters)
-    {
-        if (i.ToString() == c.ToString())
-        {
-            numberOfAppereances++;
-        }
-
-    }
-
-    Console.WriteLine(i + " => " + numberOfAppereances);
-
-
-
+    pracownik1.Print();
+}
+else if (pracownik2.Result > pracownik3.Result)
+{
+    pracownik2.Print();
+}
+else
+{
+    pracownik3.Print();
 }
